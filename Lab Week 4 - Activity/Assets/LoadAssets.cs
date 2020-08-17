05d75c65b1d617ece4e6bb018465cfcf1d007906
ChristopherCorby <69776021+ChristopherCorby@users.noreply.github.com>
2020-08-17 13:54:00 +1000

    Instantiate Prefabs - Dev 70 percent

--- Lab Week 4 - Activity/Assets/LoadAssets.cs
+++ Lab Week 4 - Activity/Assets/LoadAssets.cs
@@ -6,11 +6,16 @@ public class LoadAssets : MonoBehaviour
 {
 
     public GameObject redObj;
+    Vector3 redObjVector = new Vector3(2.0f, 0.0f, 0.0f);
+
+    [SerializeField] private GameObject blueObj;
+    Vector3 blueObjVector = new Vector3(-2.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
     {
-           
+        Instantiate(redObj, redObjVector, Quaternion.identity);
+        Instantiate(blueObj, blueObjVector, Quaternion.identity);
     }
 
     // Update is called once per frame
