74b0c27b69e41f6b2c91ee3808405c6f8acc9e94
ChristopherCorby <69776021+ChristopherCorby@users.noreply.github.com>
2020-08-17 14:43:30 +1000

    Feature Hide 100 percent

--- Lab Week 4 - Activity/Assets/PrintAndHide.cs
+++ Lab Week 4 - Activity/Assets/PrintAndHide.cs
@@ -6,11 +6,12 @@ public class PrintAndHide : MonoBehaviour
 {
     int i = 0;
     public Renderer rend;
+    int timerCount = 225;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerCount = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -18,5 +19,16 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject.name + ":" + i);
         i++;
+
+        if ((gameObject.tag == "Red") && (i >= 100))
+        {
+            gameObject.SetActive(false);
+        }
+
+        if ((gameObject.tag == "Blue") && (i == timerCount)) 
+        {
+            Debug.Log("Deleted at:" + i);
+            rend.enabled = false;
+        }
     }
 }
